4379c62344cd8264d8845647b0c565f243349020
Nicole Rauch <info@nicole-rauch.de>
2023-03-11 21:02:17 +0100

    Exercise for custom types.

---
+++ test/exercises/customTypes.test.ts
@@ -0,0 +1,21 @@
+import * as t from "io-ts";
+import * as L from "luxon";
+import {validationFailed, validationSuccessfulResultingIn} from "../helpers";
+
+
+describe("Custom Types", () => {
+
+    const Codec_CT = t.void;
+
+    it("converts an ISO DateTime string to a DateTime object", () => {
+        const s = "2022-09-17T17:59:25Z";
+
+        validationSuccessfulResultingIn(Codec_CT)(s, L.DateTime.fromISO(s));
+    });
+
+    it("fails to convert an arbitrary string", () => {
+        const s = "Hello";
+
+        validationFailed(Codec_CT)(s);
+    });
+})

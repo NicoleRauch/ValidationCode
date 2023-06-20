50703fd684427d9d3f5218ab958961dba8a6ce2d
Nicole Rauch <info@nicole-rauch.de>
2023-06-20 11:38:15 +0200

    Oops, noch ein Überbleibsel getilgt.

--- test/solutions-zod/solutions-branding.ts
+++ test/solutions-zod/solutions-branding.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 const IOArtNo = z.string().min(1).brand<"ArtNo">();
-type IArtNo = z.TypeOf<typeof IOArtNo>;
+type IArtNo = z.infer<typeof IOArtNo>;
 const artNoFor = (x: string): IArtNo => x as IArtNo;
 
 // see https://github.com/colinhacks/zod#brand

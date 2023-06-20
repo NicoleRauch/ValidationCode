69e36c0e2d7c83a01da29979f908cc2ed7270805
Nicole Rauch <info@nicole-rauch.de>
2023-06-20 12:14:29 +0200

    Kleine Verbesserungen.

--- test/solutions-zod/solutions_customTypes.ts
+++ test/solutions-zod/solutions_customTypes.ts
@@ -2,12 +2,12 @@ import * as z from "zod";
 import * as L from "luxon";
 
 export const LuxonDateTimeFromString: z.ZodEffects<z.ZodString, L.DateTime, unknown> =
-  z.string().transform((value, context) => {
+  z.string().transform((value: string, context): L.DateTime => {
     const d: L.DateTime = L.DateTime.fromISO(value);
     if(!d.isValid){
       context.addIssue({
         code: z.ZodIssueCode.custom,
-        message: `not a valid ISO-Date string: ${d.invalidExplanation}`
+          message: `not a valid ISO-Date: ${d.invalidExplanation}`
       });
       return z.NEVER;
     }
